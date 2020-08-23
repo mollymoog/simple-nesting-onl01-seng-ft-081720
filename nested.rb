557cb7281ade2772ed635655432a44de32792417
molly mk <mmkover@gmail.com>
2020-08-23 15:39:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-  hopper = programmer_hash[:grace_hopper]
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  
 end
 
 def adding_matz
